b1588f348636c19544174a0b29cc9023048d05c0
pcava <paul.cavanagh@allstate.com>
2016-06-13 16:29:19 +0100

    Update CalculatorTest.java

--- src/test/java/com/dalelotts/rpn/CalculatorTest.java
+++ src/test/java/com/dalelotts/rpn/CalculatorTest.java
@@ -1,6 +1,12 @@
 package com.dalelotts.rpn;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -13,17 +19,29 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     private Calculator calculator;
+    private static final String NEW_LINE = System.getProperty("line.separator");
 
     @Test
-    public void runShouldDoTheRightThing() {
-        // Given
-        calculator = new Calculator();
+    public void runShouldDoTheRightThing() throws Exception {
 
-        // When
+
+
+        final InputStream inputStream = IOUtils.toInputStream("1\n", "UTF-8");
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final PrintStream printStream = new PrintStream(outputStream);
+        // When splitting strings be sure to be operating system independent.
+
+
+        calculator = new Calculator(new Scanner(inputStream), printStream);
         calculator.run();
+
+        final String[] strings = outputStream.toString().split(NEW_LINE);
+
 
         // Then
         // How can we make sure the calculator did the right thing?
-        assertThat("actual", equalTo("expected"));
+        assertThat("1", equalTo(strings[2].toString()));
+
     }
+
 }
